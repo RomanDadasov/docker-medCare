8b4bcf378038263ec28806cbf79fc2a18c108859
roman <romandadasov13@gmail.com>
2026-05-19 02:05:03 +0400

    fix: CORS EC2 IP elave edildi

--- hospital-backend/HospitalManagement/HospitalManagement.Infrastructure/Extensions/ServiceExtensions.cs
+++ hospital-backend/HospitalManagement/HospitalManagement.Infrastructure/Extensions/ServiceExtensions.cs
@@ -128,7 +128,8 @@ namespace HospitalManagement.Extensions
                             "http://localhost:5173",
                             "http://127.0.0.1:5173",
                             "http://localhost:3000",
-                            "http://127.0.0.1:3000"
+                            "http://127.0.0.1:3000",
+                            "http://51.21.160.18:3000"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod()
